4cc7ae987653224b031cef2ce76bce95f90a717f
Jef Wachters <jef.wachters@gmail.com>
2015-06-20 17:11:09 +0200

    - Request offers through a form (without API call) - add capybara to have better integration test

--- app.rb
+++ app.rb
@@ -2,6 +2,21 @@ require 'sinatra'
 
 class MobileOfferApp < Sinatra::Base
   get '/' do
+    render_index
+  end
+
+  get '/mobile_offers' do
+    if !params[:uid].empty?
+      @request_successful = true
+    else
+      @request_invalid = true
+    end
+    render_index
+  end
+
+  private
+
+  def render_index
     erb :index
   end
 end
